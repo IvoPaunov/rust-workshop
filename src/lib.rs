19311f238776beca4648ca1ce0de3b953fb4beec
Ivo paunov <ivo.paunov@gmail.com>
2019-07-12 17:46:46 +0300

    use submit btn

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,30 @@
 #[macro_use]
 extern crate seed;
-use std::cmp::Ordering;
-use seed::dom_types::{El};
-use seed::prelude::*;
 use js_sys::Math::random;
+use seed::dom_types::El;
+use seed::prelude::*;
+use std::cmp::Ordering;
 
 // Model
 
+const MAX_ATTEMPTS: u32 = 3;
+
 struct Model {
 	secret_number: u32,
+	pub attempts: u32,
 	pub guess: String,
-	pub won: bool,
-	pub msg: String
+	pub game_active: bool,
+	pub msg: String,
 }
 
 impl Default for Model {
 	fn default() -> Self {
 		Self {
 			secret_number: (random() * 100.0) as u32,
+			attempts: 0,
 			msg: String::new(),
 			guess: String::new(),
-			won: false
+			game_active: true,
 		}
 	}
 }
@@ -29,28 +33,30 @@ impl Default for Model {
 
 #[derive(Clone)]
 enum Msg {
-	ChangeGuess(String)
+	ChangeGuess(String),
+	SubmitGuess
+
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut Orders<Msg>) {
 	match msg {
-		Msg::ChangeGuess (guess)=> {
-			model.guess = guess.to_string();
-
-			let parsed: u32 = match guess.trim().parse() {
+		Msg::SubmitGuess => {
+			let parsed: u32 = match model.guess.trim().parse() {
 				Ok(num) => num,
 				Err(_) => {
 					model.msg = "Sir, please enter valid integer number!".to_string();
 					return;
 				}
 			};
-			
 			model.msg = match parsed.cmp(&model.secret_number) {
 				Ordering::Less => "Too small!".to_string(),
 				Ordering::Greater => "Too big!".to_string(),
-				Ordering::Equal => "You win!".to_string()
-			};			
+				Ordering::Equal => "You win!".to_string(),				
+			};
 		},
+		Msg::ChangeGuess(guess) => {
+			model.guess = guess;
+		}
 	}
 }
 
@@ -58,13 +64,16 @@ fn update(msg: Msg, model: &mut Model, _: &mut Orders<Msg>) {
 
 fn view(model: &Model) -> El<Msg> {
 	div![
-		div![
-			format!("Your guess: {}", model.guess)
+		div![format!("Your guess: {}", model.guess)],
+		div![format!("Info: {}", model.msg)],
+		input![
+			attrs! {At::Value => model.guess},
+			input_ev(Ev::Input, Msg::ChangeGuess)
 		],
-		div![
-			format!("Info: {}", model.msg)
-		],
-		input![ attrs!{At::Value => model.guess}, input_ev(Ev::Input, Msg::ChangeGuess) ]
+		button![
+			simple_ev(Ev::Click, Msg::SubmitGuess),
+			"Submit your guess"
+		]
 	]
 }
 
